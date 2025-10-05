ac7ecbf25fece2c3fc69a214e84b38603540279f
Anand Pandey <pandeyanand260@gmail.com>
2025-10-05 23:06:56 +0530

    de

--- backend/router/expenseRouter.js
+++ backend/router/expenseRouter.js
@@ -1,26 +1,11 @@
-const { 
-  createExpense, 
-  deleteExpense, 
-  getCategoryExpense, 
-  getAllExpenses, 
-  emailSender 
-} = require('../controller/expenseController');
+const { createExpense, deleteExpense, getCategoryExpense, getAllExpenses, emailSender } = require('../controller/expenseController');
 
 const router = require('express').Router();
 
-// Add new expense
-router.post('/addExpense', createExpense);
-
-// Delete expense
-router.post('/deleteExpense', deleteExpense);
-
-// Get expenses by category
-router.get('/categoryExpense', getCategoryExpense);
-
-// Get all expenses
-router.get('/allExpenses', getAllExpenses);
-
-// Send email
-router.post('/sendEmail', emailSender);
+router.get('/addExpense',createExpense)
+router.get('/deleteExpense',deleteExpense)
+router.get('/categoryExpense',getCategoryExpense)
+router.get('/allExpenses',getAllExpenses)
+router.get('/sendEmail',emailSender);
 
 module.exports = router;
