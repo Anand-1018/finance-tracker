8f5be27ac0234297c50f47881b7668246fdca51e
Anand Pandey <pandeyanand260@gmail.com>
2025-10-05 21:41:27 +0530

    setup both locallly and production

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === "development"
+    ? process.env.VITE_REACT_APP_BACKEND_BASEURL  // local dev
+    : process.env.REACT_APP_API_URL;               // production
 
 export const axiosClient = axios.create({
-    baseURL : 'https://expensify-tracker.onrender.com'
-})
+    baseURL
+});
