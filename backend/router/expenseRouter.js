96603e0130d4ac859d15f4a8e5282e6f7b3baec1
Anand Pandey <pandeyanand260@gmail.com>
2025-10-05 23:06:04 +0530

    change expenserouter.sj

--- backend/router/expenseRouter.js
+++ backend/router/expenseRouter.js
@@ -1,11 +1,26 @@
-const { createExpense, deleteExpense, getCategoryExpense, getAllExpenses, emailSender } = require('../controller/expenseController');
+const { 
+  createExpense, 
+  deleteExpense, 
+  getCategoryExpense, 
+  getAllExpenses, 
+  emailSender 
+} = require('../controller/expenseController');
 
 const router = require('express').Router();
 
-router.get('/addExpense',createExpense)
-router.get('/deleteExpense',deleteExpense)
-router.get('/categoryExpense',getCategoryExpense)
-router.get('/allExpenses',getAllExpenses)
-router.get('/sendEmail',emailSender);
+// Add new expense
+router.post('/addExpense', createExpense);
+
+// Delete expense
+router.post('/deleteExpense', deleteExpense);
+
+// Get expenses by category
+router.get('/categoryExpense', getCategoryExpense);
+
+// Get all expenses
+router.get('/allExpenses', getAllExpenses);
+
+// Send email
+router.post('/sendEmail', emailSender);
 
 module.exports = router;
