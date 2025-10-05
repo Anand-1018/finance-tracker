b20f7e9c223c158fc3151b3f433b64628b725879
Anand Pandey <pandeyanand260@gmail.com>
2025-10-05 22:16:31 +0530

    checking console

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -4,6 +4,9 @@ const baseURL = process.env.NODE_ENV === "development"
     ? process.env.VITE_REACT_APP_BACKEND_BASEURL  // local dev
     : process.env.REACT_APP_API_URL;               // production
 
+
+    console.log("Axios Base URL:", baseURL);
+
 export const axiosClient = axios.create({
     baseURL
 });
